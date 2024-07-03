7620ecc9ee3003cd5260359149243747dc2c72a8
Mikail Cengiz <s230362@student.dtu.dk>
2024-07-03 10:32:51 +0200

    footer

---
+++ front/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='flex w-full bg-red-300 px-12 align-middle items-center h-14 justify-between'>Footer</div>
+  )
+}
+
+export default Footer
