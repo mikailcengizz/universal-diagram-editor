88dcc2be4d7e27ba3a9e78106ac343a93ae92a9e
Mikail Cengiz <s230362@student.dtu.dk>
2024-08-23 21:36:14 +0200

    mof stucture init

--- front/src/types/types.tsx
+++ front/src/types/types.tsx
@@ -24,6 +24,54 @@ export type Shape =
   | "label"
   | "compartment";
 
+// MOF structure should be used when configuring and using languages,
+// together with their visual representation types
+export type Type = {};
+
+export type Classifier = {
+  type: Type;
+  isAbstract: boolean;
+};
+
+export type Class = {
+  classifier: Classifier;
+  properties: Property[];
+  operations: Operation[];
+};
+
+export type Property = {
+  aggregation: Aggregation;
+  default: string[];
+  isComposite: boolean;
+  isDerived: boolean;
+  structuralFeature: StructuralFeature;
+  association: Association;
+};
+
+export type StructuralFeature = {
+  isReadOnly: boolean;
+  feature: Feature;
+  typedElement: TypedElement;
+  multiplicityElement: MultiplicityElement;
+};
+
+export type Association = {};
+
+export type Operation = {
+  isOrdered: boolean;
+  isUnique: boolean;
+  lower: number;
+  upper: number;
+  parameter: Parameter;
+  type: Type;
+};
+
+export type Parameter = {
+  direction: ParameterDirectionKind;
+  multiplicityElement: MultiplicityElement;
+  typedElement: TypedElement;
+};
+
 // MAPPING
 export type Mapping = { shape: Shape; metamodel: string };
 
